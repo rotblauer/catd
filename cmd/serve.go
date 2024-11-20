9ff81dd94b210290013bda15dec4e0ed651030d5
ia <isaac.ardis@gmail.com>
2024-11-20 09:11:41 -0700

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- cmd/serve.go
+++ cmd/serve.go
@@ -24,13 +24,8 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Start the webserver",
+	Long:  `Serves cat on the internet`,
 	Run: func(cmd *cobra.Command, args []string) {
 		setDefaultSlog(cmd, args)
 
