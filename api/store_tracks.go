4eb4325f25914794cddce059d582e3fa81eb885b
ia <isaac.ardis@gmail.com>
2024-12-10 10:21:28 -0700

    cats compete over master.gz
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- api/store_tracks.go
+++ api/store_tracks.go
@@ -22,17 +22,17 @@ func (c *Cat) StoreTracks(ctx context.Context, in <-chan cattrack.CatTrack) (err
 	// Sink ALL tracks (from ALL CATS) to master.geojson.gz.
 	// Cat/thread safe because gz file locks.
 	// Cat pushes will be stored in cat push/populate-batches.
-	c.logger.Info("Waiting on master locker...")
-	gzftwMaster, err := flat.NewFlatWithRoot(params.DatadirRoot).
-		NamedGZWriter(params.MasterTracksGZFileName, nil)
-	if err != nil {
-		c.logger.Error("Failed to create custom writer", "error", err)
-		select {
-		case errCh <- err:
-		default:
-		}
-		return
-	}
+	//c.logger.Info("Waiting on master locker...")
+	//gzftwMaster, err := flat.NewFlatWithRoot(params.DatadirRoot).
+	//	NamedGZWriter(params.MasterTracksGZFileName, nil)
+	//if err != nil {
+	//	c.logger.Error("Failed to create custom writer", "error", err)
+	//	select {
+	//	case errCh <- err:
+	//	default:
+	//	}
+	//	return
+	//}
 
 	truncate := flat.DefaultGZFileWriterConfig()
 	truncate.Flag = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
@@ -63,7 +63,7 @@ func (c *Cat) StoreTracks(ctx context.Context, in <-chan cattrack.CatTrack) (err
 	//stream.TeeMany(ctx, in, master, myCat, pushLast, count)
 
 	write, count := stream.Tee(ctx, in)
-	writeAll := io.MultiWriter(gzftwMaster.Writer(), gzftwLast.Writer(), cattracks.Writer())
+	writeAll := io.MultiWriter( /* gzftwMaster.Writer(), */ gzftwLast.Writer(), cattracks.Writer())
 
 	sinkStreamToJSONWriter(ctx, c, writeAll, write)
 
