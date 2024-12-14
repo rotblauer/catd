60ec3106cdb753f80a08d14259be607317c39fc3
ia <isaac.ardis@gmail.com>
2024-12-14 11:37:55 -0700

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- api/producers.go
+++ api/producers.go
@@ -12,8 +12,6 @@ func (c *Cat) ProducerPipelines(ctx context.Context, in <-chan cattrack.CatTrack
 	c.logger.Info("Producer pipelines")
 	defer c.logger.Info("Producer pipelines complete")
 
-	//return c.S2IndexTracks(ctx, in)
-
 	// Clean and improve tracks for pipeline handlers.
 	cleaned := c.CleanTracks(ctx, in)
 	improved := c.ImprovedActTracks(ctx, cleaned)
@@ -22,13 +20,14 @@ func (c *Cat) ProducerPipelines(ctx context.Context, in <-chan cattrack.CatTrack
 	areaPipeCh, vectorPipeCh := stream.Tee(ctx, woffsets)
 	groundedArea := stream.Filter[cattrack.CatTrack](ctx, clean.FilterGrounded, areaPipeCh)
 
-	errs := make(chan error, 2)
+	nPipes := 2
+	errs := make(chan error, nPipes)
 	go func() { errs <- c.S2IndexTracks(ctx, groundedArea) }()
 	go func() { errs <- c.CatActPipeline(ctx, vectorPipeCh) }()
 
 	c.logger.Info("Producer pipelines waiting for completion")
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < nPipes; i++ {
 		select {
 		case err := <-errs:
 			if err != nil {
